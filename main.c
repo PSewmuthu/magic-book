a47db4ff5df64dd8546bf0cc45fad2319e849b3e
PSewmuthu <apasindusewmuthu@gmail.com>
2023-03-14 11:28:30 +0530

    Created show_menu() function

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void show_menu(){
+    printf("----------------------- Menu -----------------------\n\n");
+    printf("Iron Man                                         01\n");
+    printf("Captain Marvel                                   02\n");
+    printf("Spider Man                                       03\n");
+    printf("Captain America                                  04\n");
+    printf("Hawkeye                                          05\n");
+    printf("Black Widow                                      06\n");
+    printf("Hulk                                             07\n");
+    printf("Doctor Strenge                                   08\n");
+    printf("Ant Man                                          09\n");
+    printf("Thor                                             10\n\n");
+}
+
 int main()
 {
     printf("Hello world!\n");
