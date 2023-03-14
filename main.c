b080b428618cf824b4333c3e57edf803eec40940
PSewmuthu <apasindusewmuthu@gmail.com>
2023-03-14 11:30:37 +0530

    Created page3() function

--- main.c
+++ main.c
@@ -64,6 +64,31 @@ int page2(){
     return choice;
 }
 
+int page3(){
+    printf("\n---------------------- Page 3 ----------------------\n\n");
+    printf("Doctor Strenge                                   08\n");
+    printf("Thor                                             10\n");
+    printf("Hawkeye                                          05\n");
+    printf("Captain Marvel                                   02\n");
+    printf("Black Widow                                      06\n");
+    printf("Spider Man                                       03\n");
+
+    char ans;
+    int choice;
+    printf("\nIs it here? (t/f) ");
+    scanf("%c", &ans);
+    printf("\n")
+
+    switch (ans){
+    case 't':
+        choice = 3;
+    default:
+        choice = 0;
+    }
+
+    return choice;
+}
+
 int main()
 {
     printf("Hello world!\n");
